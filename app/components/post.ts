c059f2a8891f3812c609e5e520451d399b32654e
Albert Groothedde <albertgroothedde@gmail.com>
2016-04-26 00:01:05 +0200

    Working example

--- app/components/post.ts
+++ app/components/post.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, OnInit } from 'angular2/core';
+import { EventEmitter, Component, Input, Output, OnInit } from 'angular2/core';
 import * as models from '../models';
 
 import {VotesComponent} from '../components/votes';
@@ -6,17 +6,24 @@ import {VotesComponent} from '../components/votes';
 @Component({
     selector: 'post',
     template: `
-    <li>
-        <votes [voteCount]="post.voteCount"></votes>
-        {{post.author}}:
-        <a href="{{post.url}}">{{post.title}}</a>
+    <li class="collection-item">
+        <p>
+          <votes [voteCount]="post.voteCount" (onVote)="onVote($event)"></votes>
+          <a href="{{post.url}}">{{post.title}}</a> <i>{{post.author}}</i>
+        </p>
     </li>
     `,
     directives: [VotesComponent]
 })
 export class PostComponent implements OnInit {
     @Input() post: models.Post;
-
+    @Input() disableAlert: boolean;
+        
     ngOnInit() { }
 
+    onVote(vote: {vote: number}) {
+      if(!!this.disableAlert){
+        alert(`Thank you for your vote (${(vote.vote > 0) ? '+'+vote.vote : vote.vote})`);
+      }
+    }
 }
--- app/components/votes.ts
+++ app/components/votes.ts
@@ -1,43 +1,43 @@
-import { Component, Input, Output, OnInit } from 'angular2/core';
+import { Component, Input, Output, EventEmitter, OnInit } from 'angular2/core';
 
 @Component({
   selector: 'votes',
   template: `
-      <div>
-          <span class="upvote" (click)="vote(+1)">^</span>
-          <span [class]="(voteCount >= 0) ? 'positiveVote' : 'negativeVote'">{{voteCount}}</span>
-          <span class="downvote" (click)="vote(-1)">v</span>
-      </div>
+      <a href="#!" (click)="vote(-1)"><i class="material-icons">thumb_down</i></a>
+      <span [class]="(voteCount >= 0) ? 'positiveVote' : 'negativeVote'">{{voteCount}}</span>
+      <a href="#!" (click)="vote(+1)"><i class="material-icons">thumb_up</i></a>
     `,
   styles: [`
       .positiveVote {
         color: white;
-        background-color: green;        
+        background-color: green;
       }
       
       .negativeVote {
         color: white;
-        background-color: red;        
+        background-color: red;
       }
     `]
 })
 export class VotesComponent implements OnInit {
   myVote: number = 0;
-  _ref: Firebase;
 
   @Input() voteCount: number;
+
+  @Output() onVote = new EventEmitter();
 
   vote(voteCount: number) {
     if (voteCount > 0) {
       if (this.myVote < 1) {
         this.myVote = this.myVote + voteCount;
         this.voteCount = this.voteCount + voteCount;
-        this._ref.update({})
+        this.onVote.next({ vote: voteCount });
       }
     } else {
       if (this.myVote > -1) {
         this.myVote = this.myVote + voteCount;
         this.voteCount = this.voteCount + voteCount;
+        this.onVote.next({ vote: voteCount });
       }
     }
   }
