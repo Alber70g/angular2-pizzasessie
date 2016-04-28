05fab1eb1d175758d578024b4d261dce270b3298
Albert Groothedde <albertgroothedde@gmail.com>
2016-04-28 21:30:23 +0200

    Root components have border

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,24 +5,35 @@ import {PostformComponent} from './components/postform';
 import {PostsListComponent} from './components/postsList';
 import { LoginComponent } from './components/login';
 import { AuthStatusComponent } from './components/auth-status';
+import { UniquePipeExampleComponent } from './components/unique-pipe-example';
 
 @Component({
   selector: 'my-app',
   template: `
     <navigation></navigation>
     <div class="container">
-      <postform></postform>
-      <postsList></postsList>
-      <login></login>
-      <auth-status></auth-status>
+      <postform class="bordered"></postform>
+      <postsList class="bordered"></postsList>
+      <login class="bordered"></login>
+      <auth-status class="bordered"></auth-status>
+      <unique-pipe-example class="bordered"></unique-pipe-example>
     </div>
     `,
+  styles: [`
+    .bordered {
+      border: 1px solid red;
+      padding: 10px;
+      margin: 10px;
+      display: block;
+    }
+  `],
   directives: [
     PostformComponent,
     NavigationComponent,
     PostsListComponent,
     LoginComponent,
-    AuthStatusComponent
+    AuthStatusComponent,
+    UniquePipeExampleComponent
   ]
 })
 
